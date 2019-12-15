4d770176e82ec7c9a8e1e44874a59ce4d27e8d00
Ayoub <ayoub.elhanchi@gmail.com>
2019-12-15 01:07:53 -0500

    sixth commit

--- examples/helpers.py
+++ examples/helpers.py
@@ -6,7 +6,7 @@ os.chdir('../')
 import torch_vr
 
 def optimize(inputs_train, targets_train, inputs_test, targets_test, model,
-             loss_func, accuracy, start, step_size, iterations=100, accelerated=False,
+             loss_func, accuracy, start, step_size, epochs=100, accelerated=False,
              momentum=0.9, stochastic=False, batch_size=128, var_reduce=None):
 
     # loads the starting point
@@ -30,38 +30,39 @@ def optimize(inputs_train, targets_train, inputs_test, targets_test, model,
     accuracies_train = list()
     accuracies_test = list()
 
-    for _ in tqdm(range(iterations)):
-
-        # computes the loss
-        if stochastic:
-            # samples the mini-batch indices uniformly without replacement
-            indices = np.random.choice(inputs_train.shape[0], batch_size, replace=False)
+    for i in tqdm(range(epochs)):
+        for j in range(int(N/batch_size)):
 
             # computes the loss
-            loss = loss_func(model(inputs_train[indices]), targets_train[indices])
-        else:
-            loss = loss_func(model(inputs_train), targets_train)
+            if stochastic:
+                # samples the mini-batch indices uniformly without replacement
+                indices = np.random.choice(inputs_train.shape[0], batch_size, replace=False)
 
-        # computes the gradient
-        loss.backward()
-
-        # constructs the gradient estimate
-        if stochastic:
-            if var_reduce not in ['SAG', 'SAGA']:
-                for p in model.parameters():
-                    p.grad *= inputs_train.shape[0]/batch_size
+                # computes the loss
+                loss = loss_func(model(inputs_train[indices]), targets_train[indices])
             else:
-                var_reducer.reduce_variance(model.outputs_grad, model.inputs, indices)
+                loss = loss_func(model(inputs_train), targets_train)
+
+            # computes the gradient
+            loss.backward()
+
+            # constructs the gradient estimate
+            if stochastic:
+                if var_reduce not in ['SAG', 'SAGA']:
+                    for p in model.parameters():
+                        p.grad *= inputs_train.shape[0]/batch_size
+                else:
+                    var_reducer.reduce_variance(model.outputs_grad, model.inputs, indices)
+
+            # takes an optimization step
+            opt.step()
+
+            # resets the gradients to 0
+            opt.zero_grad()
 
         # records the accuracy every 100 iterations
         with torch.no_grad():
             accuracies_test.append(accuracy(inputs_train, targets_test))
             accuracies_train.append(accuracy(inputs_test, targets_test))
-
-        # takes an optimization step
-        opt.step()
-
-        # resets the gradients to 0
-        opt.zero_grad()
 
     return (accuracies_train, accuracies_test)
